b57968fb9169e6a3ee734ac4bc283ecb2041b1aa
Roy Hashimoto <roy@shoestringresearch.com>
2023-12-17 11:58:14 -0800

    Add lock mode toggle.

--- app/src/main/java/com/shoestringresearch/twa/HomeActivity.kt
+++ app/src/main/java/com/shoestringresearch/twa/HomeActivity.kt
@@ -11,19 +11,39 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.KeyEvent
 import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+val IS_LOCK_TASK_KEY = booleanPreferencesKey("isLockTaskKey")
 
 const val RELOAD_DELAY_MILLIS = 2L * 60L * 1000L
+val CHEAT_CODE = listOf(
+    KeyEvent.KEYCODE_VOLUME_DOWN,
+    KeyEvent.KEYCODE_VOLUME_DOWN,
+    KeyEvent.KEYCODE_VOLUME_UP,
+    KeyEvent.KEYCODE_VOLUME_DOWN,
+)
 
 class HomeActivity: Activity() {
     private lateinit var devicePolicyManager: DevicePolicyManager
+    private val keyCodes = ArrayDeque<Int>()
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,8 +85,54 @@ class HomeActivity: Activity() {
         super.onResume()
 
         if (devicePolicyManager.isLockTaskPermitted(packageName)) {
-            Log.v("LauncherActivity", "startLockTask")
-            startLockTask()
+            runBlocking {
+                dataStore.data.map { preferences ->
+                    preferences[IS_LOCK_TASK_KEY] ?: false
+                }.first { isLockTask ->
+                    if (isLockTask) {
+                        Log.v("HomeActivity", "startLockTask")
+                        this@HomeActivity.startLockTask()
+                    } else {
+                        Log.v("HomeActivity", "stopLockTask")
+                        this@HomeActivity.stopLockTask()
+                    }
+                    true
+                }
+            }
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (event.keyCode == KeyEvent.KEYCODE_VOLUME_UP ||
+            event.keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            Log.v("HomeActivity", "${event.keyCode}")
+            handleKeyCode(event.keyCode)
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun handleKeyCode(keyCode: Int) {
+        keyCodes.addLast(keyCode)
+        while (keyCodes.size > CHEAT_CODE.size) {
+            keyCodes.removeFirst()
+        }
+
+        if (keyCodes == CHEAT_CODE) {
+            Log.v("HomeActivity", "cheat code recognized")
+            runBlocking {
+                dataStore.edit { preferences ->
+                    val isLockTask = preferences[IS_LOCK_TASK_KEY] ?: false
+                    preferences[IS_LOCK_TASK_KEY] = !isLockTask
+                    Log.v("HomeActivity", "Lock task ${!isLockTask}")
+
+                    runOnUiThread {
+                        Toast.makeText(
+                            applicationContext,
+                            "Lock task ${!isLockTask}",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
 }
