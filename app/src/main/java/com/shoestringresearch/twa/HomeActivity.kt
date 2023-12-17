536e357065aa61c35989083cac8a7cec9688542f
Roy Hashimoto <roy@shoestringresearch.com>
2023-12-17 09:42:17 -0800

    Reload on browser error.

--- app/src/main/java/com/shoestringresearch/twa/HomeActivity.kt
+++ app/src/main/java/com/shoestringresearch/twa/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.shoestringresearch.twa
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.admin.DevicePolicyManager
 import android.content.ComponentName
@@ -7,15 +8,24 @@ import android.content.Context
 import android.content.pm.ActivityInfo
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 
+const val RELOAD_DELAY_MILLIS = 2L * 60L * 1000L
+
 class HomeActivity: Activity() {
     private lateinit var devicePolicyManager: DevicePolicyManager
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -32,6 +42,7 @@ class HomeActivity: Activity() {
         webView.settings.allowContentAccess = true
         webView.settings.javaScriptEnabled = true
         webView.settings.loadsImagesAutomatically = true
+        webView.webViewClient = CustomWebViewClient(mainLooper)
         setContentView(webView)
 
         webView.loadUrl("https://rhashimoto.github.io/twa-iframe/web/dist/")
@@ -56,6 +67,47 @@ class HomeActivity: Activity() {
         if (devicePolicyManager.isLockTaskPermitted(packageName)) {
             Log.v("LauncherActivity", "startLockTask")
             startLockTask()
+        }
+    }
+}
+
+private class CustomWebViewClient(val looper: Looper): WebViewClient() {
+    val pending = HashSet<WebView>()
+    val runnable = Runnable {
+        Log.v("HomeActivity", "reloading")
+        pending.forEach {
+            it.reload()
+        }
+        pending.clear()
+    }
+
+    override fun onReceivedError(
+        view: WebView,
+        request: WebResourceRequest,
+        error: WebResourceError
+    ) {
+        super.onReceivedError(view, request, error)
+        Log.e("HomeActivity", "onReceivedError ${request.url} ${error.description}")
+        scheduleReload(view)
+    }
+
+    override fun onReceivedHttpError(
+        view: WebView,
+        request: WebResourceRequest,
+        errorResponse: WebResourceResponse
+    ) {
+        super.onReceivedHttpError(view, request, errorResponse)
+        Log.e("HomeActivity", "onReceivedHttpError ${request.url}")
+        if (!request.url.toString().endsWith("favicon.ico")) {
+            scheduleReload(view)
+        }
+    }
+
+    private fun scheduleReload(webView: WebView) {
+        Log.v("HomeActivity", "scheduleReload")
+        if (!pending.contains(webView)) {
+            pending.add(webView)
+            Handler(looper).postDelayed(runnable, RELOAD_DELAY_MILLIS)
         }
     }
 }
