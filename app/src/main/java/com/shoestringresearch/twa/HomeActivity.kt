3af14c3b0ecd640f0355994350380985be131a60
Roy Hashimoto <roy@shoestringresearch.com>
2023-12-17 07:06:50 -0800

    Replace TWA with WebView.

--- app/src/main/java/com/shoestringresearch/twa/HomeActivity.kt
+++ app/src/main/java/com/shoestringresearch/twa/HomeActivity.kt
@@ -1,24 +1,57 @@
 package com.shoestringresearch.twa
 
 import android.app.Activity
-import android.content.Intent
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.os.Build
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Log
+import android.webkit.WebView
+
 
 class HomeActivity: Activity() {
+    private lateinit var devicePolicyManager: DevicePolicyManager
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val text = TextView(this)
-        text.text = getString(android.R.string.untitled)
-        setContentView(text)
+        devicePolicyManager = getSystemService(Context.DEVICE_POLICY_SERVICE)
+            as DevicePolicyManager
+
+        // This configuration is supposed to happen in the DeviceAdminReceiver,
+        // but onEnabled() isn't called consistently when set-device-owner
+        // is invoked via adb.
+        val deviceAdmin = ComponentName(this, DeviceOwnerReceiver::class.java)
+        DeviceOwnerReceiver.configurePolicy(this, deviceAdmin)
+
+        val webView = WebView(this)
+        webView.settings.allowContentAccess = true
+        webView.settings.javaScriptEnabled = true
+        webView.settings.loadsImagesAutomatically = true
+        setContentView(webView)
+
+        webView.loadUrl("https://rhashimoto.github.io/twa-iframe/web/dist/")
+
+        requestedOrientation = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT
+        } else {
+            ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+        }
     }
 
     override fun onResume() {
         super.onResume()
 
-        // Open the TWA.
-        val intent = Intent(this, LauncherActivity::class.java)
-        startActivity(intent)
+        if (devicePolicyManager.isLockTaskPermitted(packageName)) {
+            Log.v("LauncherActivity", "startLockTask")
+            startLockTask()
+        }
+
+//        // Open the TWA.
+//        val intent = Intent(this, LauncherActivity::class.java)
+//        startActivity(intent)
     }
 }
