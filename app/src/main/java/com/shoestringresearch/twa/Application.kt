63e30cdd0771acde5ffd92d7d2ba8ecc2bc1c830
Roy Hashimoto <roy@shoestringresearch.com>
2023-12-19 16:22:26 -0800

    Convert Application subclass to Kotlin.

--- app/src/main/java/com/shoestringresearch/twa/Application.kt
+++ app/src/main/java/com/shoestringresearch/twa/Application.kt
@@ -13,17 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.shoestringresearch.twa;
+package com.shoestringresearch.twa
 
+import android.app.Application
 
-
-public class Application extends android.app.Application {
-
-  
-
-  @Override
-  public void onCreate() {
-      super.onCreate();
-      
-  }
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
 }
