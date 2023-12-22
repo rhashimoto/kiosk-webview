fdb67fecad7c9747d4113fa51d0afb68369f30b2
Roy Hashimoto <roy@shoestringresearch.com>
2023-12-21 17:32:51 -0800

    Checkpoint for authorization flow.

--- app/src/main/java/com/shoestringresearch/kiosk/webview/SettingsActivity.kt
+++ app/src/main/java/com/shoestringresearch/kiosk/webview/SettingsActivity.kt
@@ -3,11 +3,20 @@ package com.shoestringresearch.kiosk.webview
 import android.os.Bundle
 import android.text.InputFilter
 import android.text.Spanned
+import android.util.Log
+import android.widget.Button
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
+    private val authLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            Log.v("SettingActivity", "result $result")
+            handleAuthResult(result)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +28,20 @@ class SettingsActivity : AppCompatActivity() {
                 .commit()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        findViewById<Button>(R.id.auth).setOnClickListener {
+            Log.v("SettingsActivity", "onClickListener")
+            onAuthButtonClick()
+        }
+    }
+
+    private fun onAuthButtonClick() {
+        val intent = AuthActivity.createAuthIntent(application)
+        authLauncher.launch(intent)
+    }
+
+    private fun handleAuthResult(result: ActivityResult) {
+        AuthActivity.handleAuthResult(application, result)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
