f98ac8760cd47f591a6174825f5ded76afbc835a
Roy Hashimoto <roy@shoestringresearch.com>
2024-02-24 11:20:14 -0800

    Add Compose ToothbrushFragment class.

---
+++ app/src/main/java/com/shoestringresearch/kiosk/webview/ToothbrushFragment.kt
@@ -0,0 +1,29 @@
+package com.shoestringresearch.kiosk.webview
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+
+class ToothbrushFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            // Dispose the Composition when viewLifecycleOwner is destroyed
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
+            )
+
+            setContent {
+                Text("Hello Compose!")
+            }
+        }
+    }
+}
